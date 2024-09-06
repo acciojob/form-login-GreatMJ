79741b9303acf75fb44aa0f2562103edc7e687d2
Mahesh Jaybhaye <97793941+GreatMJ@users.noreply.github.com>
2024-09-06 13:09:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+  event.preventDefault();
 
+	// get form
+	const form=document.getElementById("form1");
+
+	// get elements
+	const firstName=form.elements['fname'].value;
+	const lastName=form.elements['lname'].value;
+
+	alert(` ${firstName}  ${lastName}`);
 }
